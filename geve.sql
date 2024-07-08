490ad7c28c0293bc471b30bb7fc876e8bdf23ef6
GuillermoPintoA <101642773+GuillermoPintoA@users.noreply.github.com>
2024-07-07 23:59:22 -0400

    Add files via upload

--- geve.sql
+++ geve.sql
@@ -1,110 +1,45 @@
 use geve;
-
+DROP TABLE IF EXISTS vehiculo;
 CREATE TABLE Vehiculo (
     id_vehiculo INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    comprado BOOLEAN NOT NULL,
+    patente VARCHAR(255)  NOT NULL UNIQUE,
     fechaIngreso DATE,
-    identificador VARCHAR(255),
     observacion TEXT,
     vencimientoRevision DATE,
     vencimientoPermisoCirculacion DATE,
-    obsCompra TEXT,
     ano INT,
-    numeroInterno VARCHAR(255),
-    nChasis VARCHAR(255),
+    nChasis VARCHAR(255)  NOT NULL UNIQUE,
     nMotor VARCHAR(255),
-    nCarroceria VARCHAR(255)
+    nCarroceria VARCHAR(255),
+    id_modelo INT,
+     id_comprado INT,
+     FOREIGN KEY (id_modelo) REFERENCES Modelo(id_modelo) ON DELETE CASCADE,
+    FOREIGN KEY (id_comprado) REFERENCES Comprado(id_comprado) ON DELETE CASCADE
+	
 );
+INSERT INTO Vehiculo (patente, fechaIngreso, observacion, vencimientoRevision, vencimientoPermisoCirculacion, ano, nChasis, nMotor, nCarroceria)
+VALUES 
+('AB1234',  '2021-03-15', 'Vehículo en buen estado', '2023-03-15', '2024-03-15', 2020, 'CH123456789', 'MO123456789', 'CA123456789'),
+('CD5678', '2022-05-10', 'Vehículo arrendado', '2024-05-10', '2025-05-10', 2021, 'CH987654321', 'MO987654321', 'CA987654321'),
+('EF9012', '2023-01-20', 'Vehículo en proceso de compra', '2025-01-20', '2026-01-20', 2022, 'CH112233445', 'MO112233445', 'CA112233445'),
+('GH3456', '2020-11-30', 'Vehículo usado en buen estado', '2022-11-30', '2023-11-30', 2019, 'CH556677889', 'MO556677889', 'CA556677889'),
+('IJ7890',  '2021-07-25', 'Vehículo en leasing', '2023-07-25', '2024-07-25', 2020, 'CH998877665', 'MO998877665', 'CA998877665');
 
-INSERT INTO Vehiculo (nombre, comprado, fechaIngreso, identificador, observacion, vencimientoRevision, vencimientoPermisoCirculacion, obsCompra, ano, numeroInterno, nChasis, nMotor, nCarroceria)
-VALUES ('Toyota Corolla', 1, '2023-01-15', 'TBGD34', 'Vehículo en buen estado', '2024-03-15', '2024-04-30', 'Sin observaciones', 2023, 'A123', 'CH123', 'M123', 'C123');
-
-INSERT INTO Vehiculo (nombre, comprado, fechaIngreso, identificador, observacion, vencimientoRevision, vencimientoPermisoCirculacion, obsCompra, ano, numeroInterno, nChasis, nMotor, nCarroceria)
-VALUES ('Ford Mustang', 1, '2022-06-30', 'XYZT56', 'Vehículo deportivo', '2023-12-30', '2023-08-31', 'Detalles de la compra', 2022, 'B456', 'CH456', 'M456', 'C456');
-
-INSERT INTO Vehiculo (nombre, comprado, fechaIngreso, identificador, observacion, vencimientoRevision, vencimientoPermisoCirculacion, obsCompra, ano, numeroInterno, nChasis, nMotor, nCarroceria)
-VALUES ('Honda Civic', 0, '2020-09-20', 'DEFT89', 'Vehículo en reparación', '2022-11-20', '2022-10-15', 'Compra pendiente', 2020, 'C789', 'CH789', 'M789', 'C789');
-
-
-CREATE TABLE Cargo (
-    idCargo INT AUTO_INCREMENT PRIMARY KEY,
-    subIdCargo INT,
-    idEmpresa INT,
-    nombreCargo VARCHAR(255) NOT NULL,
-    fechaCreacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    estado ENUM('Activo', 'Inactivo') NOT NULL
+CREATE TABLE cargo (
+    id_cargo INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
 );
+INSERT INTO cargo (nombre) VALUES ('Super Admin'), ('Administrador'), ('Gerente'), ('Técnico'), ('Usuario'), ('RRHH');
 
 
-CREATE TABLE Empresa (
-    idEmpresa INT AUTO_INCREMENT PRIMARY KEY,
-    rutEmpresa VARCHAR(20) NOT NULL,
-    digitoVerificador CHAR(1) NOT NULL,
-    nombreEmpresa VARCHAR(255) NOT NULL,
-    rubroEmpresa VARCHAR(255),
-    tamanoEmpresa VARCHAR(50),
-    ciudadEmpresa VARCHAR(255),
-    regionEmpresa VARCHAR(255),
-    fechaIngreso DATE,
-    rutRepresentanteLegal VARCHAR(20) NOT NULL,
-    dvRepresentanteLegal CHAR(1) NOT NULL,
-    estado ENUM('Activo', 'Inactivo') NOT NULL,
-    direccion VARCHAR(255),
-    fonoOC VARCHAR(20),
-    mailOC VARCHAR(255)
-);
-
-CREATE TABLE Marca (
-    idMarca INT AUTO_INCREMENT PRIMARY KEY,
-    nombreMarca VARCHAR(255) NOT NULL,
-    estado ENUM('Activo', 'Inactivo') NOT NULL
-);
-INSERT INTO Marca (nombreMarca, estado) VALUES
-('Toyota', 'Activo'),
-('Honda', 'Activo'),
-('Ford', 'Activo'),
-('Chevrolet', 'Activo'),
-('Nissan', 'Activo'),
-('BMW', 'Activo'),
-('Mercedes-Benz', 'Activo'),
-('Audi', 'Activo'),
-('Volkswagen', 'Activo'),
-('Hyundai', 'Activo');
-
-CREATE TABLE Modelo (
-    idModelo INT AUTO_INCREMENT PRIMARY KEY,
-    idMarca INT,
-    nombreModelo VARCHAR(255) NOT NULL,
-    estado ENUM('Activo', 'Inactivo') NOT NULL,
-    FOREIGN KEY (idMarca) REFERENCES Marca(idMarca)
-);
-
-INSERT INTO Modelo (idMarca, nombreModelo, estado) VALUES
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Toyota'), 'Corolla', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Toyota'), 'Camry', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Honda'), 'Civic', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Honda'), 'Accord', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Ford'), 'F-150', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Ford'), 'Mustang', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Chevrolet'), 'Silverado', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Chevrolet'), 'Malibu', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Nissan'), 'Altima', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Nissan'), 'Sentra', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'BMW'), '3 Series', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'BMW'), '5 Series', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Mercedes-Benz'), 'C-Class', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Mercedes-Benz'), 'E-Class', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Audi'), 'A4', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Audi'), 'Q5', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Volkswagen'), 'Golf', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Volkswagen'), 'Passat', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Hyundai'), 'Elantra', 'Activo'),
-((SELECT idMarca FROM Marca WHERE nombreMarca = 'Hyundai'), 'Santa Fe', 'Activo');
+ALTER TABLE usuario ADD COLUMN id_cargo INT DEFAULT  1;
+ALTER TABLE usuario ADD CONSTRAINT fk_cargo FOREIGN KEY (id_cargo) REFERENCES cargo(id_cargo) ON DELETE CASCADE;
 
 CREATE TABLE usuario (
     idusuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
+    apellidoP VARCHAR(255) NOT NULL,
+    apellidoM VARCHAR(255) NOT NULL,
     cargo VARCHAR(255) NOT NULL,
     mail VARCHAR(255) NOT NULL,
     fechacreacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -112,24 +47,26 @@ CREATE TABLE usuario (
     rut VARCHAR(10) NOT NULL,
     dv_rut CHAR(1) NOT NULL
 );
+ALTER TABLE usuario drop COLUMN id_cargo ;
 
-ALTER TABLE usuario ADD COLUMN nombre VARCHAR(255), ADD COLUMN apellido VARCHAR(255);
 
-INSERT INTO usuario (nombre,apellido, cargo, mail, claveweb, rut, dv_rut)
+INSERT INTO usuario (nombre,apellidoP,apellidoM, cargo, mail, claveweb, rut, dv_rut)
 VALUES
-('Guillermo','Pinto', 'Administrador', 'guillermo@gmail.com', 'password123', '20068559', '8'),
-('Juan Pérez','Pérez', 'Gerente', 'juan.perez@example.com', 'password123', '12345678', '9'),
-('María','Gómez', 'Administrador', 'maria.gomez@example.com', 'password456', '87654321', '0'),
-('Pedro','Martínez', 'Técnico', 'pedro.martinez@example.com', 'password789', '11223344', '1');
+('Guillermo','Pinto', 'Pinto','Administrador', 'guillermo@gmail.com', 'password123', '20068559', '8'),
+('Juan Pérez','Pérez', 'Pinto','Gerente', 'juan.perez@example.com', 'password123', '12345678', '9'),
+('María','Gómez','Pinto', 'Administrador', 'maria.gomez@example.com', 'password456', '87654321', '0'),
+('Pedro','Martínez', 'Pinto','Técnico', 'pedro.martinez@example.com', 'password789', '11223344', '1');
 
 CREATE TABLE historial (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_vehiculo INT,
     accion VARCHAR(50),
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    usuario VARCHAR(100)
+    usuario VARCHAR(100),
+    nombre_vehiculo VARCHAR(255),
+    patente VARCHAR(100)
 );
-ALTER TABLE historial ADD COLUMN nombre_vehiculo VARCHAR(255);
+ALTER TABLE historial ADD COLUMN patente VARCHAR(255);
 
 ALTER TABLE historial
 ADD COLUMN nombre VARCHAR(100);
@@ -151,15 +88,33 @@ CREATE TABLE reporte (
     id_vehiculo INT,
     fecha_reporte DATE,
     descripcion TEXT,
-    FOREIGN KEY (id_vehiculo) REFERENCES Vehiculo(id_vehiculo)
+    FOREIGN KEY (id_vehiculo) REFERENCES Vehiculo(id_vehiculo) ON DELETE CASCADE
 );
-
+CREATE TABLE reporte (
+    id_reporte INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_reporte DATE NOT NULL,
+    descripcion TEXT,
+    id_vehiculo INT,
+    id_motivo INT,
+    FOREIGN KEY (id_vehiculo) REFERENCES Vehiculo(id_vehiculo) ON DELETE CASCADE,
+    FOREIGN KEY (id_motivo) REFERENCES motivo(id_motivo) ON DELETE CASCADE
+);
+DROP TABLE IF EXISTS comprado;	
+CREATE TABLE reporte (
+    id_reporte INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_reporte DATE NOT NULL,
+    descripcion TEXT,
+    id_vehiculo INT,
+    id_motivo INT,
+    FOREIGN KEY (id_vehiculo) REFERENCES Vehiculo(id_vehiculo) ON DELETE CASCADE,
+    FOREIGN KEY (id_motivo) REFERENCES motivo(id_motivo) ON DELETE CASCADE
+);
 CREATE TABLE motivo (
     id_motivo INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL
 );
 
-INSERT INTO motivo (nombre) VALUES ('Falla técnica'), ('Mantenimiento programado'), ('Accidente'), ('Otro');
+INSERT INTO motivo (nombre) VALUES ('Falla técnica'), ('Falla técnica'),('Mantenimiento programado'), ('Accidente'), ('Otro');
 
 ALTER TABLE reporte ADD COLUMN id_motivo INT,
     ADD CONSTRAINT fk_reporte_motivo FOREIGN KEY (id_motivo) REFERENCES motivo(id_motivo);
@@ -171,5 +126,55 @@ CREATE TABLE Documento (
     ruta_archivo VARCHAR(255),
     tipo_archivo VARCHAR(50),
     fecha_subida DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_vehiculo) REFERENCES Vehiculo(id_vehiculo)
+    FOREIGN KEY (id_vehiculo) REFERENCES Vehiculo(id_vehiculo) ON DELETE CASCADE
 );
+CREATE TABLE Marca (
+    id_marca INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Modelo (
+    id_modelo INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    id_marca INT,
+    FOREIGN KEY (id_marca) REFERENCES Marca(id_marca) ON DELETE CASCADE
+);
+ALTER TABLE Vehiculo DROP COLUMN numeroInterno;
+-- Insertamos vehículos asociados a modelos existentes
+INSERT INTO Vehiculo (patente, fechaIngreso, observacion, vencimientoRevision, vencimientoPermisoCirculacion, ano, nChasis, nMotor, nCarroceria, id_modelo)
+VALUES
+('AB1234', 1, '2023-01-01', 'Observación 1', '2024-01-01', '2025-01-01', 2023, 'CH123456', 'M123456', 'C123456', 1),
+('CD5678', 2, '2022-05-15', 'Observación 2', '2023-05-15', '2024-05-15', 2022, 'CH654321', 'M654321', 'C654321', 2),
+('EF9012', 1, '2023-03-20', 'Observación 3', '2024-03-20', '2025-03-20', 2023, 'CH789012', 'M789012', 'C789012', 3);
+
+ALTER TABLE Vehiculo ADD id_modelo INT, ADD FOREIGN KEY (id_modelo) REFERENCES Modelo(id_modelo) ON DELETE CASCADE;
+
+INSERT INTO Marca (nombre) VALUES ('Toyota'), ('Ford'), ('Chevrolet');
+
+INSERT INTO Modelo (nombre, id_marca) VALUES 
+('Corolla', 1), ('Camry', 1), ('Hilux', 1),
+('Fiesta', 2), ('Mustang', 2), ('Ranger', 2),
+('Spark', 3), ('Cruze', 3), ('Silverado', 3);
+
+CREATE TABLE Comprado (
+    id_comprado INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(255) NOT NULL
+);
+
+INSERT INTO Comprado (tipo) VALUES ('Comprado'), ('Arrendado'), ('Otro');
+
+
+
+CREATE TABLE estado (
+    id_estado INT AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(255) NOT NULL
+);
+INSERT INTO estado (descripcion) VALUES ('Activo'), ('Inactivo');
+
+ALTER TABLE Vehiculo ADD COLUMN id_estado INT DEFAULT 1;
+ALTER TABLE Vehiculo ADD CONSTRAINT fk_estado FOREIGN KEY (id_estado) REFERENCES estado(id_estado) ON DELETE CASCADE;
+
+ALTER TABLE Vehiculo ADD COLUMN id_comprado INT, ADD FOREIGN KEY (id_comprado) REFERENCES Comprado(id_comprado) ON DELETE CASCADE;
+
+ALTER TABLE vehiculo DROP FOREIGN KEY vehiculo_ibfk_2;
+ALTER TABLE vehiculo ADD CONSTRAINT vehiculo_ibfk_2 FOREIGN KEY (id_comprado) REFERENCES comprado(id_comprado) ON DELETE CASCADE;
